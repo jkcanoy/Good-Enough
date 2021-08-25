174e4478141f99c713ed24ec039d64a952bf0917
Kyle Canoy <kcanoy@uw.edu>
2021-08-24 21:21:00 -0700

    update index.js to import new models

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,6 @@
 const User = require("./User");
+const Goal = require("./Goal");
+const Metric = require("./Metric");
+const DefaultTask = require("./DefaultTask");
 
-module.exports = { User };
+module.exports = { User, Goal, Metric, DefaultTask };
