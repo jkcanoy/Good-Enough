6733ecbdc9d7e4270c26c5297d89448784548d39
Boba Fett <thugfencer@gmail.com>
2021-08-27 01:41:29 -0700

    Changes to match new model, add queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,6 +6,7 @@ const typeDefs = gql`
     firstName: String
     lastName: String
     email: String
+    goals: [Goal]
   }
 
   type Auth {
@@ -13,8 +14,28 @@ const typeDefs = gql`
     user: User
   }
 
+  type Goal {
+    _id: ID
+    description: String
+    active: Boolean
+    tally: Int
+    date_created: String
+    date_archived: String
+    metrics: [Metric]
+  }
+
+  type Metric {
+    _id: ID
+    complete: Boolean
+    date: String
+  }
+
   type Query {
     user: User
+    goal(id: ID!): [Goal]
+    goals: [Goal]
+    metric(id: ID!): [Metric]
+    metrics: [Metric]
   }
 
   type Mutation {
@@ -31,6 +52,12 @@ const typeDefs = gql`
       password: String
     ): User
     login(email: String!, password: String!): Auth
+    createGoal(description: String, active: Boolean, tally: Int, date_created: String, date_archived: String ) : Goal
+    updateGoal(id: ID!, description: String, active: Boolean, tally: Int, date_created: String, date_archived: String ) : Goal
+    deleteGoal(id: ID!) : Goal
+    createMetric(complete: Boolean, date: String) : Metric
+    updateMetric(id: ID!, complete: Boolean, date: String) : Metric
+    deleteMetric(id: ID!) : Metric
   }
 `;
 
