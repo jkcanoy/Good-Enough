aabf6b3d1fee8ab29235845192b97efaeeafddc6
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-08-28 13:07:43 -0700

    Updated mutations and comments on queries/mutations.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,8 @@
 import { gql } from "@apollo/client";
+//CBW: 08.28.21 Queries work if date fields are commented out. Pulling single users, we run into authentication issues from GraphQL Playground that we hope are avoided by passing in authentication in header in production.
 
 export const QUERY_SINGLE_USER = gql`
-  query getSingleUser($_id: ID!) {
+  query getSingleUser($_id: ID! ) {
     user(_id: $_id) {
       _id
       firstName
@@ -63,7 +64,6 @@ export const QUERY_SINGLE_GOAL = gql`
   }
 `;
 
-// Works for everything except date fields.
 export const QUERY_GOALS = gql`
   query getGoals {
     goals {
@@ -91,7 +91,6 @@ export const QUERY_SINGLE_METRIC = gql`
   }
 `;
 
-// Works for everything except date fields.
 export const QUERY_METRICS = gql`
   query getMetrics {
       metrics {
