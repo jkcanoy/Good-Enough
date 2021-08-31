e37140d277462063863262c3447de932f17aa3fb
SPeats <srpeters44@gmail.com>
2021-08-30 21:45:00 -0700

    updated the edit goal form to remove the table elements and make it a form with columns; added the query for the goals to the page so the existing goals populate.

--- client/src/components/EditGoalForm/index.js
+++ client/src/components/EditGoalForm/index.js
@@ -1,65 +1,82 @@
-import React from "react";
+import React, { useState }from "react";
+import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { Accordion, Form, Button, Table, Card, useAccordionButton, } from "react-bootstrap";
-import CalendarUpdate from "../CalendarUpdate";
+import { Form, Button, Col, Card, } from "react-bootstrap";
+import DatePicker from "react-datepicker";
 import { QUERY_USER, QUERY_ME } from '../../utils/queries';
 
 const EditGoalForm = () => {
-    let goals = [];
+    const style = {
+        borderBottom: 'thin solid #5292ab',
+        margin: '.2em',
+    }
+
+    const { email: userParam } = useParams();
+
+    const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+      variables: { email: userParam},
+    });
+  
+    const user = data?.me || data?.user || {};
+
+    const [endDate, setEndDate] = useState( new Date() )
+
+    if (loading) {
+        return <div>Loading...</div>;
+      }
+    
+    if (!user?.email) {
+    return (
+        <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+        </h4>
+    );
+    }
 
     return (
-        <Table>
-        <thead>
-            <tr>
-            <th>Description</th>
-            <th>End Date</th>sss
-            <th>Update</th>
-            <th>Archive</th>
-            <th>Remove</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            {goals.map((goal) => (
-            <tr key={goal.id}>
-                <td>
+        <>
+        <Card.Header className="row">
+            <Col>Goal</Col>
+            <Col>Current End Date</Col> 
+            <Col>Select New End Date</Col>
+            <Col></Col>
+            <Col></Col>
+        </Card.Header>
+            {user.goals.map((goal) => (
+                <Form key={goal._id} className="row" style={style}>
+                <Col xs>
                 {goal.description}
-                </td>
-                <td>
-                    <CalendarUpdate endDate={goal.endDate}/>
-                </td>
-
-                <td>
+                </Col>
+                <Col>
+                    <DatePicker
+                        selected={endDate}
+                        selectsStart
+                        onChange={(endDate) => setEndDate(endDate)}
+                        showDisabledMonthNavigation
+                        isClearable
+                        dateFormat="MM/dd/yyyy"
+                        openToDate={endDate}
+                        withPortal
+                        />
+                </Col>
+                <Col>
                     <Button
                         type="button"
                         onClick={() => console.log("hello")}>
-                        <span role="img" aria-label="close">
-                        📝
-                        </span>
+                        Update
                     </Button>
-                </td>
-                <td>
+                </Col>
+                <Col>
                     <Button
                         type="button"
                         onClick={() => console.log("hello")}>
-                        <span role="img" aria-label="close">
-                        🦖
-                        </span>
+                        Delete
                     </Button>
-                </td>
-                <td>
-                    <Button
-                        type="button"
-                        onClick={() => console.log("hello")}>
-                        <span role="img" aria-label="close">
-                        ✖️
-                        </span>
-                    </Button>
-                </td>
-            </tr>
+                </Col>
+            </Form>
             ))}
-        </tbody>
-    </Table>
+    </>
     )
 }
 
